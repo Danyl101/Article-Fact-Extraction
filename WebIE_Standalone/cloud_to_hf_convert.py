04229bb2c143090344d728d224a5a95e97588a4c
Danyl S John <danyl.s.john@gmail.com>
2025-12-03 15:04:57 +0530

    Updated json to arrow file conversion

--- WebIE_Standalone/cloud_to_hf_convert.py
+++ WebIE_Standalone/cloud_to_hf_convert.py
@@ -8,18 +8,20 @@ from config_loader import config
 
 logger=logging.getLogger("cloud_to_hf_convert")
 
-folder = config["paths"]["WebIE"]["C4_Data_Dir_Intermediate"]
-files = [os.path.join(folder, f) for f in os.listdir(folder)]
+folder = config["paths"]["WebIE"]["C4_Data_Dir"]
 
 records = []
 
-for f in files:
-    try:
-        with open(f,encoding="utf-8") as fp:
-            records.append(json.load(fp))
-            logger.debug(f"Loaded file {f} with {len(records[-1])} records")
-    except Exception as e:
-        logger.error(f"Error loading file {f}: {e}")
-
+for root,dirs,files in os.walk(folder):
+    for filenames in files:
+        try:
+            if(filenames.endswith(".json")):
+                filepath = os.path.join(root, filenames)
+                logger.info(f"Loading file {filepath} with filename {filenames}")
+                with open(filepath,encoding="utf-8") as fp:
+                    records.append(json.load(fp))
+                    logger.debug(f"Loaded file {filenames} with {len(records[-1])} records")
+        except Exception as e:
+            logger.error(f"Error loading file {filenames}: {e}")
 ds = Dataset.from_list(records)
 ds.save_to_disk(config["paths"]["WebIE"]["C4_Data_Dir_HF"])
